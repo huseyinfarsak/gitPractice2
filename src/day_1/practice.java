8e24f377850b092e4f5b03777fbdbd220a7de7e8
huseyinfarsak <hfarsak01@hotmail.com>
2020-04-24 14:42:30 -0500

    practice

--- src/day_1/practice.java
+++ src/day_1/practice.java
@@ -3,5 +3,6 @@ package day_1;
 public class practice {
     public static void main(String[] args) {
         System.out.println("Only Practice");
+        System.out.println("I need to change");
     }
 }
