68a9532036494d88039c9520d2c374322fbfca0b
huseyinfarsak <hfarsak01@hotmail.com>
2020-04-24 15:12:01 -0500

    Asiya_Task

--- src/day_1/Asiya_Task.java
+++ src/day_1/Asiya_Task.java
@@ -3,5 +3,9 @@ package day_1;
 public class Asiya_Task {
     public static void main(String[] args) {
         System.out.println("git hub looks difficult for the first time");
+
+        int a = 5;
+        int b = 10;
+        System.out.println(a+b);
     }
 }
